eb0adcbebe204c7690118abdd9af2e17bdac2987
subhajitmandal <subhajit.mandal@nagarro.com>
2022-08-25 14:49:49 +0530

    Update code

--- HackerrankSqlPractice/Intermidiate/Contest_leaderboard.sql
+++ HackerrankSqlPractice/Intermidiate/Contest_leaderboard.sql
@@ -10,13 +10,25 @@ group by 1, 2
 having t_score > 0
 order by t_score desc, 1;
 
--- issue code on CTE
-with st (hacker_id, mx_score) as
+--  CTE - MS Sql Server
+with st (hacker_id, challenge_id, mx_score) as
     (select hacker_id, challenge_id, max(score) as mx_score
-    from submissions group by 1, 2)
-
-select st.hacker_id, h.name, sum(st.score) as t_score 
+    from submissions group by hacker_id, challenge_id
+    )
+select st.hacker_id, h.name, sum(st.mx_score) as t_score 
 from st join hackers h on st.hacker_id = h.hacker_id
-group by 1, 2
-having t_score > 0
+group by st.hacker_id, h.name
+having sum(st.mx_score) > 0
+order by t_score desc, 1;
+
+-- CTE - MySql
+with st (hacker_id,  challenge_id, mx_score) as
+    (select hacker_id, challenge_id, max(score) as mx_score
+     from submissions
+     group by hacker_id, challenge_id
+    )
+select st.hacker_id, h.name, sum(st.mx_score) as t_score
+from st join hackers h on st.hacker_id = h.hacker_id
+group by st.hacker_id, h.name
+having sum(st.mx_score) > 0
 order by t_score desc, 1;
